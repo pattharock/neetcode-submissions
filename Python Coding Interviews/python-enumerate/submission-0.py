9961b83ff28fb603cce671565afbc99fea3f0fd2
Ritvik Singh <48458898+pattharock@users.noreply.github.com>
2026-06-07 17:35:49 +0800

    Add: python-enumerate - submission-0

---
+++ Python Coding Interviews/python-enumerate/submission-0.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def get_index_of_seven(nums: List[int]) -> int:
+    for i, n in enumerate(nums):
+        if n == 7:
+            return i
+    return -1
+
+
+def get_dist_between_sevens(nums: List[int]) -> int:
+    distance = 0
+    occurance = -1
+
+    for i, n in enumerate(nums):
+        if n == 7:
+            occurance += 1
+        
+        if occurance == 0:
+            distance += 1
+        elif occurance == 1:
+            return distance
+    return distance
+
+
+
+# do not modify below this line
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 8, 9]))
+print(get_index_of_seven([2, 4, 7, 5, 7, 8, 4, 2]))
+
+print(get_dist_between_sevens([1, 2, 7, 4, 5, 6, 7, 8, 9]))
+print(get_dist_between_sevens([2, 7, 7, 7, 8]))
+print(get_dist_between_sevens([7, 4, 8, 4, 2, 7]))
