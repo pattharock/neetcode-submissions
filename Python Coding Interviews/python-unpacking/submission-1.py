3822e5fa54889fe68c432e735cc8a409d5712f65
Ritvik Singh <48458898+pattharock@users.noreply.github.com>
2026-08-09 00:27:55 +0800

    Add: python-unpacking - submission-1

---
+++ Python Coding Interviews/python-unpacking/submission-1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def sort_words(words: List[str]) -> List[str]:
+    return sorted(words)
+
+def sort_numbers(nums: List[int]) -> List[int]:
+    return sorted(nums, key=lambda x: abs(x), reverse=True)
+
+original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]
+
+print(original_words)
+print(sort_words(original_words))
+
+original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]
+
+print(original_numbers)
+print(sort_numbers(original_numbers))
