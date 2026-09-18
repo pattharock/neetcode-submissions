4fdc0ce5654bd9a55d3ca5b52415eec3bc379ab8
Ritvik Singh <48458898+pattharock@users.noreply.github.com>
2026-09-18 18:44:04 -0400

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def prefixOfTwo(self, s: str, t:str) -> int:
+        if len(s) > len(t):
+            s, t = t, s
+    
+        i = 0
+
+        while (i < min(len(s), len(t))):
+            if s[i] == t[i]:
+                i += 1
+            else:
+                break
+        return i
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort(key=lambda x: len(x))
+
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+            prefix_bound = self.prefixOfTwo(prefix, strs[i])
+            prefix = prefix[:prefix_bound]
+        
+        return prefix
+            
